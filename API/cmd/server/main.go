57f25fd91494b8a8ad8eee193c0c99d4c0fa83ff
SavioCAlves <savio6.bcc@proton.me>
2024-06-20 19:44:53 -0300

    Estudos do dia 20 de junho

--- API/cmd/server/main.go
+++ API/cmd/server/main.go
@@ -1,8 +1,72 @@
 package main
 
-import "APIS/configs"
+import (
+	"APIS/configs"
+	"APIS/internal/entity"
+	"APIS/internal/infra/database"
+	"APIS/internal/infra/webserver/handlers"
+	"net/http"
+
+	_ "APIS/docs"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/jwtauth"
+	httpSwagger "github.com/swaggo/http-swagger"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+// @title 					Go Experti API Example
+// @version					1.0
+// @description				Product API with authentication
+
+// @contact.name			Sávio Alves
+// @contact.url				http://itprotect.com.br
+// @termsOfService			http://swagger.io/terms/
+
+// @license.name			Full Cycle License
+// @license.url				http://www.fullcycle.com.br
+
+// @host					localhost:8000
+// @BasePath				/
+// @securityDefinitions.apikey	ApiKeyAuth
+// @in header
+// @name Authorization
 
 func main() {
-	config, _ := configs.LoadConfig(".")
-	println(config.DBDriver)
+	configs, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&entity.Product{}, &entity.User{})
+	productDB := database.NewProduct(db)
+	productHandler := handlers.NewProductHandler(productDB)
+
+	userDB := database.NewUser(db)
+	userHandler := handlers.NewUserHandler(userDB, configs.TokenAuth, configs.JWTExpiresIn)
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.Route("/products", func(r chi.Router) {
+		r.Use(jwtauth.Verifier(configs.TokenAuth))
+		r.Use(jwtauth.Authenticator)
+		r.Post("/", productHandler.CreateProduct)
+		r.Get("/{id}", productHandler.GetProduct)
+		r.Get("/", productHandler.GetProducts)
+		r.Put("/{id}", productHandler.UpdateProduct)
+		r.Delete("/{id}", productHandler.DeleteProduct)
+	})
+
+	r.Post("/users", userHandler.Create)
+	r.Post("/users/generate_token", userHandler.GetJWT)
+
+	r.Get("/docs/*", httpSwagger.Handler(httpSwagger.URL("http://localhost:8000/docs/doc.json")))
+
+	http.ListenAndServe(":8000", r)
 }
